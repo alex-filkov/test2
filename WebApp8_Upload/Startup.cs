6ed389c68ebd4b040584883c5c14b14cd39e48d7
AlexandreFilkov <AlexandreFilkov@I7TCWS20>
2017-02-20 10:32:20 -0800

    Add project files.

---
+++ WebApp8_Upload/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp8_Upload.Startup))]
+namespace WebApp8_Upload
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
